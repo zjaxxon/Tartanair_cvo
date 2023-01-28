9de73ea7e7c7f842f6e3a1a9c013c22679f5c4b7
Jaxxon <zjaxxon@gmail.com>
2023-01-27 22:36:05 -0500

    update

--- est_mv.sh
+++ est_mv.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for i in abandonedfactory abandonedfactory_night endofworld carwelding amusement gascola hospital japanesealley neighborhood ocean office oldtown seasidetown seasonsforest seasonsforest_winter soulcity westerndesert
 do
-	echo $i	
-	evo_rpe kitti ./ref/pose_left_${i}_kitti.txt ./est/$i.txt -va --plot --plot_mode xz --save_results results/${i}.zip
+    echo $i	
+    mv ./est/cvo_tartan_outdoor_$i.txt $i.txt
 done
